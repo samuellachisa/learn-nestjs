9bb82e6babb9389c10a526adf7225b78e6c2d121
Samuel Lachisa <Samilalet9@gmail.com>
2024-03-14 18:27:53 +0300

    feat: docker initialize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+import { CarModule } from './car/car.module';
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CarController } from './car/car.controller';
 import { CarService } from './car/car.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    CarModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Abcd@12345',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [CarController],
   providers: [AppService, CarService],
 })
